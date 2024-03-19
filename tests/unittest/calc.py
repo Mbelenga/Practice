16b506757a6507e97e733532e5b35900aa39d418
Mbelenga <mbelengaterence@gmail.com>
2024-03-19 16:57:31 +0300

    Updated

---
+++ tests/unittest/calc.py
@@ -0,0 +1,15 @@
+def add (x, y):
+    """Adds a function"""
+    return x+y
+
+def subtract(x,y):
+    """Subtracts a function"""
+    return x-y
+
+def multiply(x,y):
+    """Multiplies a function"""
+    return x*y
+
+def divide(x,y):
+    """DIVID
+
